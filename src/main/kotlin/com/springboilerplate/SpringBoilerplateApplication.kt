db44f291cdbcfcbdc2b912fac4d36d1f1352205c
Namgyu11 <103015031+Namgyu11@users.noreply.github.com>
2024-11-13 00:02:05 +0900

    Initial commit

---
+++ src/main/kotlin/com/springboilerplate/SpringBoilerplateApplication.kt
@@ -0,0 +1,11 @@
+package com.springboilerplate
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBoilerplateApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringBoilerplateApplication>(*args)
+}
